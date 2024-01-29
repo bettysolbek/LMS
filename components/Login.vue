fa84ac5d9bfa7fb10a6f92595afbf2a6abdfa19f
bettysolbek <bettysolbekr@gmail.com>
2024-01-29 11:27:44 +0300

    sidebar added

--- components/Login.vue
+++ components/Login.vue
@@ -1,7 +1,3 @@
-<!-- <template>
-    <h1>hi</h1>
-</template> -->
-
 <template>
   <div class="mx-auto flex">
     <div class="w-1/2 tuto min-h-screen rounded-tr-xl rounded-bl-xl animate"> 
@@ -23,9 +19,9 @@
           <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
           <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your password" required>
         </div>
-        <Nuxtlink to="admin.vue">
+        <nuxt-link to="/admin">
          <button type="submit" class="w-full text-white tuto hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-        </Nuxtlink>  
+        </nuxt-link>  
       </form>
     </div>
   </div>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3000', 
-  timeout: 10000, 
+  baseURL: process.env.BASE_URL || 'http://localhost:5000', 
+  timeout: 50000, 
 });
 
 export default ({ app }, inject) => {
